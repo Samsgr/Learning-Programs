7bb75f31e20f1d6021b81d1d6d2fcebfa28c3057
Samsgr <samruddhisgr3@gmail.com>
2025-09-10 15:52:30 +0530

    feat: add Student class program in Java

---
+++ C_Programs/C_Programs/Python_Basics/Python_Basics/Java_OOPs/Student.java
@@ -0,0 +1,20 @@
+// Simple Student class in Java
+
+class Student {
+    String name;
+    int age;
+
+    Student(String n, int a) {
+        name = n;
+        age = a;
+    }
+
+    void display() {
+        System.out.println("Name: " + name + ", Age: " + age);
+    }
+
+    public static void main(String[] args) {
+        Student s1 = new Student("Sam", 20);
+        s1.display();
+    }
+}
